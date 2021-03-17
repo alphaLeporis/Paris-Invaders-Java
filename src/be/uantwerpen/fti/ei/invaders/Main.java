a890bd4e620558c2a5d1f1d75f0da129ab8805b7
⍺ Leporis <louis.delooze@gmail.com>
2021-03-17 22:15:53 +0100

    adding the gameloop and initial display

--- src/be/uantwerpen/fti/ei/invaders/Main.java
+++ src/be/uantwerpen/fti/ei/invaders/Main.java
@@ -1,8 +1,12 @@
 package be.uantwerpen.fti.ei.invaders;
 
+import be.uantwerpen.fti.ei.invaders.gameEngine.GameLoop;
+import be.uantwerpen.fti.ei.invaders.graphicsEngine.Display;
+import be.uantwerpen.fti.ei.invaders.graphicsEngine.Game;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        new Thread(new GameLoop(new Game(800, 600))).start();
     }
 }
---
+++ src/be/uantwerpen/fti/ei/invaders/graphicsEngine/Display.java
@@ -0,0 +1,37 @@
+package be.uantwerpen.fti.ei.invaders.graphicsEngine;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+public class Display extends JFrame {
+    private Canvas canvas;
+
+    public Display(int width, int height) {
+        setTitle("Paris Invaders");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+
+        canvas = new Canvas();
+        canvas.setPreferredSize(new Dimension(width, height));
+        canvas.setFocusable(false);
+        add(canvas);
+        pack();
+
+        canvas.createBufferStrategy(3);
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public void render(Game game) {
+        BufferStrategy bufferStrategy = canvas.getBufferStrategy();
+        Graphics graphics = bufferStrategy.getDrawGraphics();
+
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(0,0, canvas.getWidth(),canvas.getHeight());
+
+        graphics.dispose();
+        bufferStrategy.show();
+    }
+}
